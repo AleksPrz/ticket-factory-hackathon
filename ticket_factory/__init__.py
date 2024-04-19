13ec21f054bbf9567e91e2686bec59501a858c31
AleksPrz <alexis4g2@outlook.com>
2024-04-19 00:23:01 -0600

    web_ticket: url fixed on views.py. Static files references changed in ticket.html

--- ticket_factory/__init__.py
+++ ticket_factory/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from . import wallet, web_ticket, factory
 
@@ -19,5 +19,9 @@ def create_app():
     app.register_blueprint(web, url_prefix = "/view", static_folder = "static/web_ticket")
     app.register_blueprint(api, url_prefix = "/api")
 
+    #Allows to send static files such as images and javascript files in a secure way
+    @app.route('/static/<path:path>')
+    def send_static(path):
+        return send_from_directory('static', path) 
 
     return app
